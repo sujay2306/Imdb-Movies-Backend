99c750571683c8cf2bb90146b663b3e36162586c
sujay2306 <sujayks007@gmail.com>
2023-04-21 11:23:50 +0530

    added movies

---
+++ src/main/java/com/example/movies/Movie.java
@@ -0,0 +1,28 @@
+package com.example.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collation ="movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String poster;
+    private String trailerLink;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<Review> reviewIds;
+
+}
